680ceed65c30c6f75b0744871782e2f10a29ce19
afonsopaulo <100703217+afonsopaulo@users.noreply.github.com>
2022-07-21 12:11:25 -0300

    git init
    Clone da netflix

---
+++ js/owl/main.js
@@ -0,0 +1,102 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+
+function a1(){
+    window.open("https://animefire.net/animes/kanojo-okarishimasu-todos-os-episodios")
+}
+
+function a2(){
+    window.open("https://animefire.net/animes/overlord-dublado-todos-os-episodios")
+}
+
+function a3(){
+    window.open("https://animefire.net/animes/youkoso-jitsuryoku-shijou-shugi-no-kyoushitsu-e-tv-todos-os-episodios")
+}
+
+function a4(){
+    window.open("https://animefire.net/animes/boruto-naruto-next-generations-dublado-todos-os-episodios")
+}
+
+function a5(){
+    window.open("https://animefire.net/animes/tate-no-yuusha-no-nariagari-todos-os-episodios")
+}
+
+function a6(){
+    window.open("https://animefire.net/animes/dungeon-ni-deai-wo-motomeru-no-wa-machigatteiru-darou-ka-todos-os-episodios")
+}
+
+function a7(){
+    window.open("https://animefire.net/animes/kaguya-sama-wa-kokurasetai-ultra-romantic-dublado-todos-os-episodios")
+}
+
+function a8(){
+    window.open("https://animefire.net/animes/fullmetal-alchemist-brotherhood-dublado-todos-os-episodios")
+}
+
+function a9(){
+    window.open("https://animefire.net/animes/steins-gate-dublado-todos-os-episodios")
+}
+
+function a10(){
+    window.open("https://animefire.net/animes/shingeki-no-kyojin-dublado-todos-os-episodios")
+}
+
+function a11(){
+    window.open("https://animefire.net/animes/hunter-x-hunter-2011-todos-os-episodios")
+}
+
+function a12(){
+    window.open("https://animefire.net/animes/violet-evergarden-movie-dublado-todos-os-episodios")
+}
+
+function a13(){
+    window.open("https://animefire.net/animes/owarimonogatari-todos-os-episodios")
+}
+
+function a14(){
+    window.open("https://animefire.net/animes/kimetsu-no-yaiba-yuukaku-hen-todos-os-episodios")
+}
+
+function a15(){
+    window.open("https://animefire.net/animes/spy-x-family-dublado-todos-os-episodios")
+}
+
+function as(){
+    window.open("https://animefire.net/animes/sword-art-online-progressive-movie---hoshi-naki-yoru-no-aria-todos-os-episodios")
+}
+
+function trailer(){
+    window.open("https://youtu.be/xjjFCxurylA")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
